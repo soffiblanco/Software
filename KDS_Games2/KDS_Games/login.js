f1e5a5754c6e51132060d74925827ea727b88364
soffiblanco <msblanco@unis.edu.gt>
2024-05-05 00:24:51 -0600

    login nuevo con verificacion

--- KDS_Games2/KDS_Games/login.js
+++ KDS_Games2/KDS_Games/login.js
@@ -17,56 +17,82 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-login.addEventListener('click', (e)=>{
+const loginBtn = document.getElementById('login');
+const cerrarBtn = document.getElementById('cerrar');
+const registro = document.getElementById('registro');
+const registroForm = document.getElementById('registro-form');
+
+registroForm.style.display = 'none';
+
+registro.addEventListener('click', (e)=>{
+    e.preventDefault();
+    registroForm.style.display='block';
+})
+
+loginBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
     var email = document.getElementById('emaillog').value;
     var password = document.getElementById('passwordlog').value;
 
     signInWithEmailAndPassword(auth, email, password).then(cred =>{
-        alert ("Usuario logueado");
-    }).catch(error => {
-        const errorCode = error.code;
-
-        if(errorCode == 'auth/Invalid-email')
-        alert('El correo no es válido');
-        else if(errorCode == 'auth/user-disabled')
-        alert('El usuario ha sido deshabilitado');
-        else if(errorCode == 'auth/user-not-found')
-        alert('El usuario no existe');
-        else if(errorCode == 'auth/wrong-password')
-        alert('Contraseña incorrecta');
-    });
-});
-
-cerrar.addEventListener('click', (e)=>{
-    auth.signOut().then(()=>{
-        alert('Sesión iniciada');
-    }).catch((error)=>{
-        alert('Error al cerrar sesión');
-    });
-})
-
-auth.onAuthStateChanged(user=>{
-    if(user){
-        console.log("Usuario activo");
-        var email = user.emailVerified;
-        if(email){
-            window.open("https://www.google.com/")
+        showSuccessMessage ("Usuario logueado");
+        if(auth.currentUser.emailVerified){
+            window.open("https://www.google.com/");
         } else{
             auth.signOut();
         }
-    }else{
-        console.log("Usuario inactivo");
-    }
+    }).catch(error => {
+        console.log('Código de error: ', error.code);
+        const errorCode = error.code;
+
+        switch(error.code){
+            case 'auth/invalid-email':
+                showErrorMessage('El correo no es válido');
+                break;
+            case 'auth/user-disabled':
+                showErrorMessage('El usuario ha sido deshabilitado');
+                break;
+            case 'auth/user-not-found':
+                showErrorMessage('El usuario no existe');
+            case 'auth/wrong-password':
+                showErrorMessage('Contraseña incorrecta');
+                break;
+            case 'auth/weak-password':
+                showErrorMessage('La contraseña debe tener al menos 6 caracteres');
+                break;
+            default:
+                showErrorMessage('Se ha producido un error');
+                break;
+        }
+        
+    });
 });
 
-const btnLoginPopup = document.querySelector('.btnLogin-popup');
-const loginPopup = document.querySelector('.login-popup');
-const btnCerrar = document.querySelector('#cerrar');
+cerrarBtn.addEventListener('click', (e)=>{
+    auth.signOut().then(()=>{
+        showSuccessMessage('Sesión cerrada');
+    }).catch((error)=>{
+        showErrorMessage('Error al cerrar sesión');
+    });
+})
 
-btnLoginPopup.addEventListener('click', () => {
-    loginPopup.classList.add('active');
-});
 
-btnCerrar.addEventListener('click', () => {
-    loginPopup.classList.remove('active');
-});
+function showSuccessMessage(message) {
+    const successMessage = document.createElement('div');
+    successMessage.textContent = message;
+    successMessage.classList.add('success-message');
+    document.body.appendChild(successMessage);
+    setTimeout(() => {
+        successMessage.remove();
+    }, 3000);
+}
+
+function showErrorMessage(message) {
+    const errorMessage = document.createElement('div');
+    errorMessage.textContent = message;
+    errorMessage.classList.add('error-message');
+    document.body.appendChild(errorMessage);
+    setTimeout(() => {
+        errorMessage.remove();
+    }, 3000);
+}
--- KDS_Games2/KDS_Games/registro.js
+++ KDS_Games2/KDS_Games/registro.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
+
 import {sendEmailVerification, getAuth, signInWithPopup, 
     createUserWithEmailAndPassword, signInWithEmailAndPassword,  
     onAuthStateChanged} from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js';
@@ -16,39 +17,53 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-const showRegistroBtn = document.getElementById('showRegistroForm');
-const registroScreen = document.getElementById('registro-screen');
-
-showRegistroBtn.addEventListener('click', () => {
-    registroScreen.style.display = 'block';
-});
-
-
-
-const handleRegistro = (e)=>{
-    e.preventDefault();
+registro.addEventListener('click', (e)=>{
     var email = document.getElementById('emailreg').value;
     var password = document.getElementById('passwordreg').value;
 
     createUserWithEmailAndPassword(auth, email, password).then(cred =>{
-        alert ("Usuario creado");
+        showSuccessMessage("Usuario creado");
         sendEmailVerification(auth.currentUser).then(()=>{
-            alert('Se ha enviado un correo de verificación');
+            showSuccessMessage('Se ha enviado un correo de verificación');
         });
     }).catch(error => {
+        console.error('Error code: ', error.code);
         const errorCode = error.code;
 
-        if(errorCode == 'auth/email.already-in-use')
-        alert('El correo ya está en uso');
-        else if(errorCode == 'auth/invalid-email')
-        alert('El correo no es válido');
-        else if(errorCode == 'auth/weak-password')
-        alert('La contraseña debe tener al menos 6 caracteres');
-    }); 
-};
+        switch(errorCode){
+            case 'auth/email-already-in-use':
+                showErrorMessage('El correo ya está en uso');
+                break;
+            case 'auth/invalid-email':
+                showErrorMessage('El correo no es válido');
+                break;
+            case 'auth/weak-password':
+                showErrorMessage('La contraseña debe tener al menos 6 caracteres');
+                break;
+            default:
+                showErrorMessage('Hubo un error');
+                break;
+        }
 
+    })
+})
 
+function showSuccessMessage(message) {
+    const successMessage = document.createElement('div');
+    successMessage.textContent = message;
+    successMessage.classList.add('success-message');
+    document.body.appendChild(successMessage);
+    setTimeout(() => {
+        successMessage.remove();
+    }, 3000);
+}
 
-
-const registroBtn = document.getElementById('registro');
-registroBtn.addEventListener('click', handleRegistro);
+function showErrorMessage(message) {
+    const errorMessage = document.createElement('div');
+    errorMessage.textContent = message;
+    errorMessage.classList.add('error-message');
+    document.body.appendChild(errorMessage);
+    setTimeout(() => {
+        errorMessage.remove();
+    }, 3000);
+}
